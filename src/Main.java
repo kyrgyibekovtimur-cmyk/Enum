59074ee8445beded3ce4595ed4ffce84995b8695
timur <kyrgyibekovtimur@gmail.com>
2026-01-13 13:28:24 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 public class Main {
      public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+         System.out.println("Write Day");
 
-         Days day = Days.TUESDAY;
+         String d = scanner.nextLine().toUpperCase();
 
-
+Days day = Days.valueOf(d);
 
              switch (day) {
                  case MONDAY:
